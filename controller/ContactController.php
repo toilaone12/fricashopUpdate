d55754fe01773e0e08995e9d7f9c2bb4970c619b
lehaitu <haitu1702@gmail.com>
2022-12-18 18:02:25 +0700

    tu first commit

--- controller/ContactController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once "../classes/contact.php";
-    class ContactController{
-        public function contact(){
-            $contact = new Contact();
-            $c = $contact->get_contact();
-            return $c;
-        }
-    }
